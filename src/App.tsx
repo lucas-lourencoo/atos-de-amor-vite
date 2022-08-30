ff645027956275b072ae1061fb98ca7befba2bbe
Lucas Lourenço <lucascelestiano@gmail.com>
2022-08-30 10:59:33 -0400

    Scroll top on change route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { BrowserRouter } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import { PostsContextProvider } from "./contexts/PostsContext";
 import { Router } from "./Routes";
 import "./styles/global.scss";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <PostsContextProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Router />
       </BrowserRouter>
     </PostsContextProvider>
